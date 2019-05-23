38abd551e92f903fc2fc775892f9a6d0d9c4e54c
Ryan Purcell <ryan.purcell@gexaenergy.com>
2019-05-23 17:01:58 -0500

    feat(token): Added method that allows the generation of a token based on input
    New method for generating tokens based on name and expiration time.  Must have the claim Can_Generate_Token on the token using the method.

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ const refresh_private_pem = new Buffer(process.env.JWT_PRIVATE_REFRESH_KEY);
 const refresh_public_pem = new Buffer(process.env.JWT_PUBLIC_REFRESH_KEY);
 const audience_list = process.env.CUSTOMER_AUDIENCE_LIST.split(',');
 const _ = require('lodash');
+const jwt_authorization = require('../lib/jwt_authorization');
+const validation_helper = require('../lib/helpers/validation.helper');
 
 const standard_options = function (subject, expires_in = '30 min') {
     return {
@@ -17,6 +19,35 @@ const standard_options = function (subject, expires_in = '30 min') {
         subject: subject
     };
 };
+
+router.post('/generate',
+    jwt_authorization.middleware({
+        audience: process.env.AUDIENCE,
+        issuer: [process.env.INTERNAL_ISSUER]
+    }),
+    jwt_authorization.verify_claims('Can_Generate_Token', true),
+    validation_helper.validation_middleware('generate_token_request'),
+    function (req, res, next) {
+        try {
+            let options = {
+                algorithm: 'RS256',
+                audience: audience_list,
+                issuer: process.env.INTERNAL_ISSUER,
+                expiresIn: '1 day',
+                subject: req.generate_token_request.Name
+            };
+            if (req.generate_token_request.Expires_In) {
+                options.expiresIn = req.generate_token_request.Expires_In;
+            }
+            const access_token = jwt.sign({}, token_private_pem, options);
+            options.expiresIn = '1 week';
+            const refresh_token = jwt.sign({}, refresh_private_pem, options);
+            res.send({access_token: access_token, refresh_token: refresh_token});
+        }
+        catch (err) {
+            next(err);
+        }
+    });
 
 router.post('/refresh', function (req, res, next) {
     try {
@@ -35,44 +66,46 @@ router.post('/refresh', function (req, res, next) {
     }
 });
 
-router.post('/', async function (req, res, next) {
-    try {
-        let response = await external_api.post('/api/user/authenticate', req.body);
-        let user_creds = response.body;
-        let payload = {
-            Email: user_creds.Profile.Email_Address,
-            claims: {}
-        };
+router.post('/',
+    validation_helper.validation_middleware('login_request'),
+    async function (req, res, next) {
+        try {
+            let response = await external_api.post('/api/user/authenticate', req.body);
+            let user_creds = response.body;
+            let payload = {
+                Email: user_creds.Profile.Email_Address,
+                claims: {}
+            };
 
-        if (user_creds.Account_permissions){
-            let customer_account = _.find(user_creds.Account_permissions, function(permission){
-                return permission.AccountType === 'Customer_Account_Id';
-            });
-            let service_accounts = _.filter(user_creds.Account_permissions, function(permission){
-                return permission.AccountType === 'Service_Account_Id';
-            });
-            if (customer_account){
-                payload.claims.Customer_Account_Id = customer_account.AccountNumber;
-            }
-            if (service_accounts){
-                payload.claims.Service_Account_Ids = _.map(service_accounts, function(service_account){
-                    return service_account.AccountNumber;
+            if (user_creds.Account_permissions) {
+                let customer_account = _.find(user_creds.Account_permissions, function (permission) {
+                    return permission.AccountType === 'Customer_Account_Id';
                 });
+                let service_accounts = _.filter(user_creds.Account_permissions, function (permission) {
+                    return permission.AccountType === 'Service_Account_Id';
+                });
+                if (customer_account) {
+                    payload.claims.Customer_Account_Id = customer_account.AccountNumber;
+                }
+                if (service_accounts) {
+                    payload.claims.Service_Account_Ids = _.map(service_accounts, function (service_account) {
+                        return service_account.AccountNumber;
+                    });
+                }
+            }
+
+            let token = jwt.sign(payload, token_private_pem, standard_options(user_creds.Profile.Username));
+            let refresh_token = jwt.sign(payload, refresh_private_pem, standard_options(user_creds.Profile.Username, '1 week'));
+            res.send({access_token: token, refresh_token: refresh_token});
+        }
+        catch (err) {
+            if (err.stack && err.status && err.status < 500) {
+                res.status(err.status).send(err.response.body.Message);
+            }
+            else {
+                next(err);
             }
         }
-
-        let token = jwt.sign(payload, token_private_pem, standard_options(user_creds.Profile.Username));
-        let refresh_token = jwt.sign(payload, refresh_private_pem, standard_options(user_creds.Profile.Username, '1 week'));
-        res.send({access_token: token, refresh_token: refresh_token});
-    }
-    catch (err) {
-        if (err.stack && err.status && err.status < 500) {
-            res.status(err.status).send(err.response.body.Message);
-        }
-        else {
-            next(err);
-        }
-    }
-});
+    });
 
 module.exports = router;
