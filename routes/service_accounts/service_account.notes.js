924a45890de23b3f566023fa4a7d00478f5d104d
Kalpana Bansikar <kalpana.bansikar@gexaenergy.com>
2019-05-31 15:51:27 -0500

    feat(getCSPNotes): GMMMINOR-88:Create API to pull the last X notes

---
+++ routes/service_accounts/service_account.notes.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router({mergeParams: true});
+const jwt_authorization = require("../../lib/jwt_authorization");
+const service_account_service = require('../../lib/services/service_account_service');
+
+router.get('/:maxRetRec',
+    jwt_authorization.verify_claims_from_request_property('Service_Account_Ids', 'params.id'),
+    async function (req, res, next) {
+        try {
+
+            let result = await service_account_service.get_notes(req.params.id, req.params.maxRetRec);
+            res.send(result);
+        }
+        catch (err) {
+            next(err);
+        }
+    });
+
+module.exports = router;
